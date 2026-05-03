c25b922ff19f2d4b8f8c36dd84071d02b9a5dc58
Md-Yousuf-sheikh <sheikhyousuf702@gmail.com>
2026-05-03 17:32:58 +0600

    add: assets

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -6,24 +6,82 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home'), elevation: 0),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        toolbarHeight: 90,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Icon(Icons.shopping_bag, size: 64, color: Colors.green),
-            const SizedBox(height: 16),
-            const Text(
-              'Welcome to Nectar',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'Your grocery store at fingertips',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
+            Image.asset('assets/icons/header_icon.png', height: 28),
+            const SizedBox(height: 6),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.location_pin, color: Colors.black, size: 20),
+                SizedBox(width: 4),
+                Text(
+                  'Dhaka, Banassre',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
+      ),
+
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search Store',
+                prefixIcon: const Icon(Icons.search),
+                filled: true,
+                fillColor: const Color(0xFFF2F3F2),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 14,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 40),
+
+          const Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.shopping_bag, size: 64, color: Colors.green),
+                  SizedBox(height: 16),
+                  Text(
+                    'Welcome to Nectar',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Your grocery store at fingertips',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
