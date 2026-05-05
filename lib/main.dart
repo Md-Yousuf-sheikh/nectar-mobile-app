c83f9e6bbdd01529080e90b22ce87479c3f95831
Md-Yousuf-sheikh <sheikhyousuf702@gmail.com>
2026-05-05 16:34:16 +0600

    fix: update splash screen assets and configuration
    - Changed the default splash screen image in flutter_native_splash.yaml from assets/splash.png to assets/icon.png.
    - Updated splash images for various Android drawable resolutions to ensure consistency across devices.
    - Adjusted LaunchScreen storyboard dimensions for iOS to better fit the new splash image.
    - Commented out the previous system UI overlay settings in main.dart to allow for manual system UI mode management.

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,16 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.black,
-      statusBarIconBrightness: Brightness.light, // Android
-      statusBarBrightness: Brightness.dark, // iOS
-    ),
+  // SystemChrome.setSystemUIOverlayStyle(
+  //   const SystemUiOverlayStyle(
+  //     statusBarColor: Colors.black,
+  //     statusBarIconBrightness: Brightness.dark, // Android
+  //     statusBarBrightness: Brightness.dark, // iOS
+  //   ),
+  // );
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: SystemUiOverlay.values,
   );
 
   final counterProvider = CounterProvider();
